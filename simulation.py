eea550084c7b1ff8dbfb00231a4003c8beacf327
tuongminh11 <84221704+tuongminh11@users.noreply.github.com>
2023-09-08 18:05:57 +0700

    Add files via upload
    xem thử này dc chưa

--- simulation.py
+++ simulation.py
@@ -2,60 +2,84 @@ import paho.mqtt.client as mqtt
 from datetime import datetime
 import json
 from random import random
-import random
 import time
 
-ChargingMode: False
-Mode: True
+class TemperatureSensor:
+    sensor_type='temperature'
+    units='celcius'
+    instance_id='SN123'
 
-
-class SoC_demo:
-    def __init__(self, min_SoC, max_SoC):
-        self.min_SoC = min_SoC
-        self.max_SoC = max_SoC
-        self.SoC_value = 0
+    def __init__(self, average_temperature, temperature_variation, min_temperature, max_temperature):
+        self.average_temperature=average_temperature
+        self.temperature_variation=temperature_variation
+        self.min_temperature=min_temperature
+        self.max_temperature=max_temperature
+        self.value=0.0
 
     def sense(self):
-        self.SoC_value = self.first_SoC_val()
-        return self.SoC_value
+        self.value= self.simple_random()
+        return self.value
 
-    def first_SoC_val(self):
-        SoC_value = random.randint(self.min_SoC, self.max_SoC)
-        return SoC_value
-
-
-def on_message(client, userdata, message):
-    # In ra thông tin tin nhắn
-    print("Topic:", message.topic)
-    print("Payload:", message.payload.decode())
-    data = json.loads(message.payload.decode())
-    Mode = data['ChargingMode']
-    return Mode, data
+    def simple_random(self):
+        value = self.min_temperature + (random() * (self.max_temperature - self.min_temperature))
+        return value
 
 
 class Simulator:
     def __init__(self, interval):
         self.interval = interval
 
+    begin = True
+
     def start(self):
+        ts= TemperatureSensor(20,10,16,35)
+
+        def on_message(client, userdata, message):
+            print('Message topic {}'.format(message.topic))
+            print('Message payload:')
+            print(message.payload.decode())
+            sig = message.payload.decode()
+            if sig == '0':
+                self.begin = False 
+
         mqtt_publisher = mqtt.Client('Temperature publisher')
-        mqtt_publisher.subscribe("evse_service/EVSE45678/#")
         mqtt_publisher.on_message = on_message
-        mqtt_publisher.connect('171.244.57.88', 1883, 60)
-        print(message.payload.decode())
+        mqtt_publisher.connect('127.0.0.1',1883,60)
         mqtt_publisher.loop_start()
 
-        # while on_message.Mode == True:
-        #     SoC_ran = SoC_demo(20, 95)
-        #     SoC_send = SoC_ran.sense()
-        #     while SoC_send < 95:
-        #         SoC_send = SoC_send + 1
-        #         SoC_messege = {"SoC": SoC_send}
-        #         jmsg1 = json.dumps(SoC_messege, indent=1)
-        #         mqtt_publisher.publish('evse_service/EVSE45678/SoC', jmsg1, 2)
-        #         time.sleep(1)
-        #     time.sleep(self.interval)
+
+        while self.begin:
+            dt = datetime.now().strftime("%d-%m-%YT%H:%M:%S")
+            message = {
+                "type-id": "de.uni-stuttgart.iaas.sc." + ts.sensor_type,
+                "instance-id": ts.instance_id,
+                "timestamp": dt,
+                "value":{
+                ts.units: ts.sense()
+                }
+            }
+
+            jmsg = json.dumps(message, indent=4)
+            mqtt_publisher.publish('TopicID', jmsg, 2)
+
+            print(ts.sensor_type , ts.sense())
+            time.sleep(self.interval)
+            mqtt_publisher.subscribe('TopicID/stop', qos=2)
 
 
-s = Simulator(2)
-s.start()
+def on_message(client, userdata, message):
+    print('Message topic {}'.format(message.topic))
+    print('Message payload:')
+    print(message.payload.decode())
+    sig = message.payload.decode()
+    if sig == '0':
+        mqtt_subscriber.disconnect() 
+        s = Simulator(5)
+        s.start() 
+
+mqtt_subscriber = mqtt.Client('Temperature subscriber')
+mqtt_subscriber.on_message = on_message
+mqtt_subscriber.connect('127.0.0.1',1883,60)
+mqtt_subscriber.subscribe('TopicID', qos=2)
+
+mqtt_subscriber.loop_forever()
